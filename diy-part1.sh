f3374c7c37a7f6841a4d2d36d18616f09f52f334
Komorebi <njxzc1219@gmail.com>
2023-02-09 16:00:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+### 修改主题文件
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
