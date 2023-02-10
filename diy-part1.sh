9fe5f7c0632c6947cd84bf9ae680a33659fc629f
Komorebi <njxzc1219@gmail.com>
2023-02-10 15:12:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-### Add feed sources
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/Zxilly/UA2F package/UA2F
+### 添加第三方订阅源（这边建议编译后去更改成中科大的源）
+# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+# git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+# git clone https://github.com/Zxilly/UA2F package/UA2F
 
 ### 修改主题文件
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
