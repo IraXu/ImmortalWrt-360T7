c9c0e517f73524d0a8dbeaebb1e34785fed13674
Komorebi <njxzc1219@gmail.com>
2023-02-10 19:28:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,3 @@
 # sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 # git clone https://github.com/Zxilly/UA2F package/UA2F
-
-### 修改主题文件
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
