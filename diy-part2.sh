e0b4df5c6e32e5fd826215e9a5066d7b99a7cd1f
Komorebi <njxzc1219@gmail.com>
2023-02-11 21:23:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+###
+ # @Author: xiawang1024
+ # @Date: 2023-02-11 21:21:21
+ # @LastEditTime: 2023-02-11 21:22:22
+ # @LastEditors: IraXu
+ # @Description: 
+ # @FilePath: /ImmortalWrt-360T7/diy2-part2.sh
+ # 开源让世界美好
+### 
 
 # 自定义默认网关，后方的192.168.10.1即是可自定义的部分
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # 固件版本名称自定义
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu ($time) '/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
