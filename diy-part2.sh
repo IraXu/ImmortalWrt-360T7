e14238514c80957dea1e40381ba641c7e8f3dd21
Komorebi <njxzc1219@gmail.com>
2023-02-15 17:16:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: xiawang1024
  # @Date: 2023-02-11 21:21:21
- # @LastEditTime: 2023-02-11 21:22:22
+ # @LastEditTime: 2023-02-25 17:16:22
  # @LastEditors: IraXu
  # @Description: 
  # @FilePath: /ImmortalWrt-360T7/diy2-part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
 
 # 添加 cpufreq（cpu性能调节优化）---- 实验性功能，可能打包失败
-# rm -rf package/lean/luci-app-cpufreq
+# rm -rf package/luci-app-cpufreq
 # svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq feeds/luci/applications/luci-app-cpufreq
 # ln -sf ../../../feeds/luci/applications/luci-app-cpufreq ./package/feeds/luci/luci-app-cpufreq
 # sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
