ba1603e4222cc580a056861e23b27ecede84d318
Komorebi <njxzc1219@gmail.com>
2023-03-22 16:55:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,6 @@
 ### 
 
 # 自定义默认网关，后方的192.168.77.1即是可自定义的部分
-sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 # 固件版本名称自定义
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
