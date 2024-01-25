9b2ed47578947254bcb24a2acabbc7aaf952a5ca
Komorebi <njxzc1219@gmail.com>
2024-01-25 14:55:47 +0800

    修改第第三方订阅源

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-### 添加第三方订阅源（这边建议编译后去更改成中科大的源）
-# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
-# git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+### 添加第三方订阅源
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
