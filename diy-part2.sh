d244752b4fde87eb1215f6a8f60a0f530d8352c7
Komorebi <njxzc1219@gmail.com>
2026-03-26 23:08:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-###
- # @Author: xiawang1024
- # @Date: 2023-02-11 21:21:21
- # @LastEditTime: 2023-02-25 17:16:22
- # @LastEditors: IraXu
- # @Description: 
- # @FilePath: /ImmortalWrt-360T7/diy2-part2.sh
- # 开源让世界美好
-### 
 
-# 自定义默认网关，后方的192.168.77.1即是可自定义的部分
-sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
-# 固件版本名称自定义
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g" package/base-files/files/etc/openwrt_release
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.77.1/' ./package/base-files/files/bin/config_generate
+sed -i 's/hostname='ImmortalWrt'/hostname='360T7'/' ./package/base-files/files/bin/config_generate
+sed -i 's/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g' ./package/base-files/files/etc/openwrt_release
