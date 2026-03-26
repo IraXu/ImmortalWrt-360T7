65f71fc94527f624d76d8709525d878c94b24669
Komorebi <njxzc1219@gmail.com>
2026-03-26 23:31:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.77.1/' ./package/base-files/files/bin/config_generate
-sed -i 's/hostname='ImmortalWrt'/hostname='360T7'/' ./package/base-files/files/bin/config_generate
-sed -i 's/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d") '/g' ./package/base-files/files/etc/openwrt_release
+sed -i 's/192.168.1.1/192.168.77.1/g' ./package/base-files/files/bin/config_generate
+sed -i 's/hostname='ImmortalWrt'/hostname='360T7'/g' ./package/base-files/files/bin/config_generate
+sed -i 's/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By IraXu $(date +"%Y%m%d")'/g' ./package/base-files/files/etc/openwrt_release
